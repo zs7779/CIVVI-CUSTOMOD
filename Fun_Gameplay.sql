b78827901b3f018da1ac32b05e1dd21a933106df
zs7779 <zs7779@hotmail.com>
2018-02-13 15:58:13 -0500

    Add files via upload

---
+++ Fun_Gameplay.sql
@@ -0,0 +1,11 @@
+﻿INSERT INTO TraitModifiers
+(	TraitType,						ModifierId							)	VALUES
+(	'FIRST_EMPEROR_TRAIT',			'TRAIT_ADJUST_BUILDER_CHARGES_MORE'	);
+
+INSERT INTO Modifiers
+(	ModifierId,								ModifierType,										RunOnce,	Permanent,	SubjectRequirementSetId		)	VALUES
+(	'TRAIT_ADJUST_BUILDER_CHARGES_MORE',	'MODIFIER_PLAYER_UNITS_ADJUST_BUILDER_CHARGES',		'0',		'0',		'UNIT_IS_BUILDER'			);
+
+INSERT INTO ModifierArguments
+(	ModifierId,								Name,			Value	)	VALUES
+(	'TRAIT_ADJUST_BUILDER_CHARGES_MORE',	'Amount',		'4'		);
